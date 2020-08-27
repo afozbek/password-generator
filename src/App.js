11c294e6acd47361008d50e131b2ac2287b191ba
afozbek <furkanozbek1995@gmail.com>
2020-08-27 23:23:59 +0300

    clipboard a kopyalama tanımlandı

--- src/App.js
+++ src/App.js
@@ -1,68 +1,124 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from "react";
 
 import ClipboardIcon from "./components/icons/ClipboardIcon";
-
-
-
+import { useToast } from "@chakra-ui/core";
 
 function App() {
+  const [password, setPassword] = useState("tC5%gO1.iS0!pQ3wD6/");
+  const [copyBtnText, setCopyBtnText] = useState("COPY");
   const [passwordLength, setPasswordLength] = useState(10);
   const [includeUppercase, setIncludeUppercase] = useState(true);
   const [includeLowercase, setIncludeLowercase] = useState(true);
   const [includeNumbers, setIncludeNumbers] = useState(false);
   const [includeSymbols, setIncludeSymbols] = useState(false);
 
-  const handleCopyPassword = (e) => {
-    console.log("Copying password")
-  }
+  const toast = useToast();
+  const copyBtn = useRef();
 
   const handleGeneratePassword = (e) => {
     console.log("Generating password");
-  }
+  };
+
+  const copyToClipboard = () => {
+    const newTextArea = document.createElement("textarea");
+    newTextArea.innerText = password;
+    document.body.appendChild(newTextArea);
+    newTextArea.select();
+    document.execCommand("copy");
+    newTextArea.remove();
+
+    setCopyBtnText("COPIED");
+    copyBtn.current.disabled = true;
+    setTimeout(() => {
+      setCopyBtnText("COPY");
+      copyBtn.current.disabled = false;
+    }, 3000);
+  };
+
+  const handleCopyPassword = (e) => {
+    console.log("Copying password");
+
+    copyToClipboard();
+  };
 
   return (
     <div className="m-container">
       <div className="m-generator">
-          <h2 className="m-generator__header">
-            Password Generator
-          </h2>
+        <h2 className="m-generator__header">Password Generator</h2>
 
-          <div className="m-generator__password">
-            tC5%gO1.iS0!pQ3wD6/
+        <div className="m-generator__password">
+          {password}
+          <button
+            className="m-generator__passwordGenerateBtn"
+            onClick={handleCopyPassword}
+            ref={copyBtn}
+          >
+            {/* <ClipboardIcon /> */}
+            {copyBtnText}
+          </button>
+        </div>
 
-            <button className="m-generator__passwordGenerateBtn" onClick={handleCopyPassword}>
-              <ClipboardIcon />
-            </button>
-          </div>
+        <div className="form-group">
+          <label htmlFor="password-length">Password length</label>
+          <input
+            name="password-length"
+            id="password-length"
+            type="number"
+            max="20"
+            min="5"
+            defaultValue={passwordLength}
+            onChange={(e) => setPasswordLength(e.target.value)}
+          />
+        </div>
 
-          <div className="form-group">
-            <label htmlFor="password-length">Password length</label>
-            <input name="password-length" id="password-length" type="number"  max="20" min="5" defaultValue={passwordLength} onChange={(e)=>setPasswordLength(e.target.value)}/>
-          </div>
+        <div className="form-group">
+          <label htmlFor="uppercase-letters">Include uppercase letters</label>
+          <input
+            id="uppercase-letters"
+            name="uppercase-letters"
+            type="checkbox"
+            checked={includeUppercase}
+            onChange={(e) => setIncludeUppercase(e.target.checked)}
+          />
+        </div>
 
-          <div className="form-group">
-            <label htmlFor="password-length">Include uppercase letters</label>
-            <input name="uppercase-letters" type="checkbox" checked={includeUppercase} onChange={(e) => setIncludeUppercase(e.target.checked)}/>
-          </div>
+        <div className="form-group">
+          <label htmlFor="lowercase-letters">Include lowercase letters</label>
+          <input
+            id="lowercase-letters"
+            name="lowercase-letters"
+            type="checkbox"
+            checked={includeLowercase}
+            onChange={(e) => setIncludeLowercase(e.target.checked)}
+          />
+        </div>
 
-          <div className="form-group">
-            <label htmlFor="lowercase-letters">Include lowercase letters</label>
-            <input name="lowercase-letters" type="checkbox" checked={includeLowercase} onChange={(e) => setIncludeLowercase(e.target.checked)} />
-          </div>
+        <div className="form-group">
+          <label htmlFor="include-numbers">Include Numbers</label>
+          <input
+            id="include-numbers"
+            name="include-numbers"
+            type="checkbox"
+            checked={includeNumbers}
+            onChange={(e) => setIncludeNumbers(e.target.checked)}
+          />
+        </div>
 
-          <div className="form-group">
-            <label htmlFor="include-numbers">Include Numbers</label>
-            <input name="include-numbers" type="checkbox" checked={includeNumbers} onChange={(e) => setIncludeNumbers(e.target.checked)}/>
-          </div>
+        <div className="form-group">
+          <label htmlFor="include-symbols">Include Symbols</label>
+          <input
+            id="include-symbols"
+            name="include-symbols"
+            type="checkbox"
+            checked={includeSymbols}
+            onChange={(e) => setIncludeSymbols(e.target.checked)}
+          />
+        </div>
 
-          <div className="form-group">
-            <label htmlFor="include-symbols">Include Symbols</label>
-            <input name="include-symbols" type="checkbox" checked={includeSymbols} onChange={(e) => setIncludeSymbols(e.target.checked)}/>
-          </div>
-
-          <button className="m-generator__btn" onClick={handleGeneratePassword}>Generate Password</button>
+        <button className="m-generator__btn" onClick={handleGeneratePassword}>
+          Generate Password
+        </button>
       </div>
-
     </div>
   );
 }
